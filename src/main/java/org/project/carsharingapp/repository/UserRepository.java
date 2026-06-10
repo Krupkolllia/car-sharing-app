94e47dac5d58ad9beaf9923629b61c33f51ac8bf
Krupkolllia <krupkoilla397@gmail.com>
2026-06-11 00:56:56 +0200

    feat(user): add existsByEmail method to repository

--- src/main/java/org/project/carsharingapp/repository/UserRepository.java
+++ src/main/java/org/project/carsharingapp/repository/UserRepository.java
@@ -5,5 +5,8 @@ import org.project.carsharingapp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+
     Optional<User> findByEmail(String email);
 }
