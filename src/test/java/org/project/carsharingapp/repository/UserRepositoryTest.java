0ee347eb14a97a5179c9e168efe58511409b5afd
Krupkolllia <krupkoilla397@gmail.com>
2026-06-15 15:56:47 +0200

    test(user): test user repository

--- src/test/java/org/project/carsharingapp/repository/UserRepositoryTest.java
+++ src/test/java/org/project/carsharingapp/repository/UserRepositoryTest.java
@@ -1,9 +1,20 @@
 package org.project.carsharingapp.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.project.carsharingapp.util.TestDataHelper.ADD_SCRIPT_PATH;
+import static org.project.carsharingapp.util.TestDataHelper.DELETE_SCRIPT_PATH;
+import static org.project.carsharingapp.util.TestDataHelper.createTestCustomer;
+
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.project.carsharingapp.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase.Replace;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = Replace.NONE)
@@ -12,6 +23,76 @@ public class UserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
 
+    @Test
+    @Sql(scripts = ADD_SCRIPT_PATH, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = DELETE_SCRIPT_PATH, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("""
+        findByEmail method with email of existing User in database
+        should return not empty Optional<User> with 1 User with
+        requested email
+        """)
+    public void findByEmail_WithValidEmail_ShouldReturnOptionalWithUser() {
+        // Given
+        User expected = createTestCustomer();
 
+        // When
+        Optional<User> actual = userRepository.findByEmail(expected.getEmail());
 
+        // Then
+        assertThat(actual).isPresent().get().isEqualTo(expected);
+    }
+
+    @Test
+    @Sql(scripts = ADD_SCRIPT_PATH, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = DELETE_SCRIPT_PATH, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("""
+        findByEmail method with email of non-existing User in database
+        should return empty Optional<User>
+        """)
+    public void findByEmail_WithInvalidEmail_ShouldReturnEmptyOptional() {
+        // Given
+        String invalidEmail = "invalidEmail@mail.com";
+
+        // When
+        Optional<User> actual = userRepository.findByEmail(invalidEmail);
+
+        // Then
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    @Sql(scripts = ADD_SCRIPT_PATH, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = DELETE_SCRIPT_PATH, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("""
+        existsByEmail method with email of existing User
+        in database should return boolean true
+        """)
+    public void existsByEmail_WithValidEmail_ShouldReturnBooleanTrue() {
+        // Given
+        String email = createTestCustomer().getEmail();
+
+        // When
+        boolean actual = userRepository.existsByEmail(email);
+
+        // Then
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    @Sql(scripts = ADD_SCRIPT_PATH, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+    @Sql(scripts = DELETE_SCRIPT_PATH, executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+    @DisplayName("""
+        existsByEmail method with email of non-existing
+        User in database should return boolean false
+        """)
+    public void existsByEmail_WithInvalidEmail_ShouldReturnBooleanFalse() {
+        // Given
+        String invalidEmail = "invalidEmail@mail.com";
+
+        // When
+        boolean actual = userRepository.existsByEmail(invalidEmail);
+
+        // Then
+        assertThat(actual).isFalse();
+    }
 }
--- src/test/java/org/project/carsharingapp/util/TestDataHelper.java
+++ src/test/java/org/project/carsharingapp/util/TestDataHelper.java
@@ -1,4 +1,20 @@
 package org.project.carsharingapp.util;
 
+import org.project.carsharingapp.model.Role;
+import org.project.carsharingapp.model.User;
+
 public class TestDataHelper {
+    public static final String ADD_SCRIPT_PATH = "classpath:database/add-test-data.sql";
+    public static final String DELETE_SCRIPT_PATH = "classpath:database/delete-test-data.sql";
+
+    public static User createTestCustomer() {
+        return new User()
+            .setId(1L)
+            .setEmail("test.user1@mail.com")
+            .setPassword("$2a$12$DtQUdqgM9yq4yrtbnDpFiO/9NlyzGlhoIBNIgx1Njak.NB7SniGRi")
+            .setFirstName("test")
+            .setLastName("user1")
+            .setRole(Role.CUSTOMER)
+            .setDeleted(false);
+    }
 }
