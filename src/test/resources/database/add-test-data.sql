c04a76ada6d8e2fe150bc53cf07542ae357c4abd
Krupkolllia <krupkoilla397@gmail.com>
2026-06-15 14:19:25 +0200

    test(db): create add and delete test data scripts

---
+++ src/test/resources/database/add-test-data.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, email, password, first_name, last_name, role)
+VALUES (1, 'test.user1@mail.com', '$2a$12$DtQUdqgM9yq4yrtbnDpFiO/9NlyzGlhoIBNIgx1Njak.NB7SniGRi',
+        'test', 'user1', 'CUSTOMER'),
+       (2, 'test.user2@mail.com', '$2a$12$pxEsgyMwS5exaM8cUp/9I.Op555.VknQYlR5v5ZPzhF/7kT/yP6xe',
+        'test', 'user2', 'CUSTOMER'),
+       (3, 'test.manager1@mail.com', '$2a$12$4XBo1Uaq3WFdgEnoRT.6H..Y3X.cj6A5kaUqkaIg8Y64RgC.ux2Om',
+        'test', 'manager1', 'MANAGER');
+
