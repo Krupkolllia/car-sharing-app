c8f07b982c403d241c226781942c838c487718f3
Krupkolllia <krupkoilla397@gmail.com>
2026-06-20 15:52:01 +0200

    chore(db): change test users passwords

--- src/test/resources/database/add-test-data.sql
+++ src/test/resources/database/add-test-data.sql
@@ -1,9 +1,10 @@
 DELETE FROM users;
 INSERT INTO users (id, email, password, first_name, last_name, role)
-VALUES (1, 'test.user1@mail.com', '$2a$12$DtQUdqgM9yq4yrtbnDpFiO/9NlyzGlhoIBNIgx1Njak.NB7SniGRi',
+VALUES (1, 'test.user1@mail.com', '$2a$12$TAdN8UgihLS/va7duaWPpuMiwNKY9osRtyLtMQhOqUtIjSkg0AMfK',
         'test', 'user1', 'CUSTOMER'),
-       (2, 'test.user2@mail.com', '$2a$12$pxEsgyMwS5exaM8cUp/9I.Op555.VknQYlR5v5ZPzhF/7kT/yP6xe',
+       (2, 'test.user2@mail.com', '$2a$12$oYswZ6gUaAkZ.XsupQcVx.gzKGrUzVzsFMnOGKvFB2HieSgrLoYSi',
         'test', 'user2', 'CUSTOMER'),
-       (3, 'test.manager1@mail.com', '$2a$12$4XBo1Uaq3WFdgEnoRT.6H..Y3X.cj6A5kaUqkaIg8Y64RgC.ux2Om',
+       (3, 'test.manager1@mail.com', '$2a$12$qKflh2qMWNcDITE/LS3XbOM55ijO1M16.tkgZyrfESWiHTmM1aNni',
         'test', 'manager1', 'MANAGER');
 
+SELECT setval('users_id_seq', (SELECT MAX(id) FROM users));
