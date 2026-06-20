4c86859371c0c02b9a37ed57b2ba15e2513f5da0
Krupkolllia <krupkoilla397@gmail.com>
2026-06-20 15:52:52 +0200

    refactor(test): add user password constant to test data helper

--- src/test/java/org/project/carsharingapp/util/TestDataHelper.java
+++ src/test/java/org/project/carsharingapp/util/TestDataHelper.java
@@ -7,11 +7,13 @@ public class TestDataHelper {
     public static final String ADD_SCRIPT_PATH = "classpath:database/add-test-data.sql";
     public static final String DELETE_SCRIPT_PATH = "classpath:database/delete-test-data.sql";
 
+    public static final String USER_RAW_PASSWORD = "testuser1";
+
     public static User createTestCustomer() {
         return new User()
             .setId(1L)
             .setEmail("test.user1@mail.com")
-            .setPassword("$2a$12$DtQUdqgM9yq4yrtbnDpFiO/9NlyzGlhoIBNIgx1Njak.NB7SniGRi")
+            .setPassword("$2a$12$KvTBaCc8tnqLRp3F0c1Bp.DZZYUf0TUmLdcNdnt/w2uPdQZ/5l1m6")
             .setFirstName("test")
             .setLastName("user1")
             .setRole(Role.CUSTOMER)
