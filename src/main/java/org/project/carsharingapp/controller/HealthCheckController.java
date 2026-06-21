98c0a3f2afde92451365d387559a528897cd3839
Krupkolllia <krupkoilla397@gmail.com>
2026-06-22 00:52:10 +0200

    feat(infra): add swagger tag to health check controller

--- src/main/java/org/project/carsharingapp/controller/HealthCheckController.java
+++ src/main/java/org/project/carsharingapp/controller/HealthCheckController.java
@@ -1,10 +1,12 @@
 package org.project.carsharingapp.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Health check")
 @RestController
 @RequestMapping("/health")
 public class HealthCheckController {
