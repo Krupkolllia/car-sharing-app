3587a1397183dc915cf0b7e1950e70c00adc8508
Krupkolllia <krupkoilla397@gmail.com>
2026-06-22 19:30:36 +0200

    test(car): test car service

--- src/test/java/org/project/carsharingapp/util/TestDataHelper.java
+++ src/test/java/org/project/carsharingapp/util/TestDataHelper.java
@@ -1,5 +1,10 @@
 package org.project.carsharingapp.util;
 
+import java.math.BigDecimal;
+import org.project.carsharingapp.dto.car.CarRequestDto;
+import org.project.carsharingapp.dto.car.CarResponseDto;
+import org.project.carsharingapp.model.car.Car;
+import org.project.carsharingapp.model.car.CarType;
 import org.project.carsharingapp.model.user.Role;
 import org.project.carsharingapp.model.user.User;
 
@@ -18,5 +23,27 @@ public class TestDataHelper {
             .setLastName("user1")
             .setRole(Role.CUSTOMER)
             .setDeleted(false);
+    }
+
+    public static CarRequestDto createCarRequestDto() {
+        return new CarRequestDto(
+            "M5", "BMW", CarType.SEDAN.name(), 1, new BigDecimal("39.99")
+        );
+    }
+
+    public static Car createCar() {
+        return new Car()
+            .setModel("M5")
+            .setBrand("BMW")
+            .setType(CarType.SEDAN)
+            .setInventory(1)
+            .setDailyFee(new BigDecimal("39.99"))
+            .setDeleted(false);
+    }
+
+    public static CarResponseDto createCarResponseDto() {
+        return new CarResponseDto(
+            null, "M5", "BMW", CarType.SEDAN.name(), 1, new BigDecimal("39.99")
+        );
     }
 }
