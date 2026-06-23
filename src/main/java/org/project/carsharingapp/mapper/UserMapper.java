cdf45531caf30b2565c5132f304afd52a16ee181
Krupkolllia <krupkoilla397@gmail.com>
2026-06-23 22:51:51 +0200

    feat(user): create new user mapper methods

--- src/main/java/org/project/carsharingapp/mapper/UserMapper.java
+++ src/main/java/org/project/carsharingapp/mapper/UserMapper.java
@@ -1,9 +1,13 @@
 package org.project.carsharingapp.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.project.carsharingapp.config.MapStructConfig;
 import org.project.carsharingapp.dto.auth.UserRegisterRequestDto;
 import org.project.carsharingapp.dto.auth.UserResponseDto;
+import org.project.carsharingapp.dto.user.UpdateUserProfileDto;
+import org.project.carsharingapp.dto.user.UpdateUserRoleDto;
+import org.project.carsharingapp.dto.user.UserProfileDto;
 import org.project.carsharingapp.model.user.User;
 
 @Mapper(config = MapStructConfig.class)
@@ -12,5 +16,11 @@ public interface UserMapper {
     UserResponseDto toDto(User user);
 
     User toModel(UserRegisterRequestDto requestDto);
+
+    UserProfileDto toProfileDto(User user);
+
+    void update(@MappingTarget User user, UpdateUserProfileDto updateDto);
+
+    void update(@MappingTarget User user, UpdateUserRoleDto updateDto);
 
 }
