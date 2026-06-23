62f88977ca1a84c10fde9d41ce69ebbecf3c0129
Krupkolllia <krupkoilla397@gmail.com>
2026-06-24 01:48:48 +0200

    feat(user): implement user service

--- src/main/java/org/project/carsharingapp/service/UserService.java
+++ src/main/java/org/project/carsharingapp/service/UserService.java
@@ -1,4 +1,15 @@
 package org.project.carsharingapp.service;
 
+import org.project.carsharingapp.dto.user.UpdateUserProfileDto;
+import org.project.carsharingapp.dto.user.UpdateUserRoleDto;
+import org.project.carsharingapp.dto.user.UserProfileDto;
+
 public interface UserService {
+
+    UserProfileDto getProfile();
+
+    UserProfileDto updateProfile(UpdateUserProfileDto updateDto);
+
+    UserProfileDto updateUserRole(Long id, UpdateUserRoleDto updateDto);
+
 }
--- src/main/java/org/project/carsharingapp/service/impl/UserServiceImpl.java
+++ src/main/java/org/project/carsharingapp/service/impl/UserServiceImpl.java
@@ -1,4 +1,47 @@
 package org.project.carsharingapp.service.impl;
 
-public class UserServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.project.carsharingapp.dto.user.UpdateUserProfileDto;
+import org.project.carsharingapp.dto.user.UpdateUserRoleDto;
+import org.project.carsharingapp.dto.user.UserProfileDto;
+import org.project.carsharingapp.exception.EntityNotFoundException;
+import org.project.carsharingapp.mapper.UserMapper;
+import org.project.carsharingapp.model.user.User;
+import org.project.carsharingapp.repository.UserRepository;
+import org.project.carsharingapp.security.SecurityUtil;
+import org.project.carsharingapp.service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserMapper userMapper;
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserProfileDto getProfile() {
+        return userMapper.toProfileDto(SecurityUtil.getAuthenticatedUser());
+    }
+
+    @Override
+    public UserProfileDto updateProfile(UpdateUserProfileDto updateDto) {
+        User user = SecurityUtil.getAuthenticatedUser();
+
+        userMapper.update(user, updateDto);
+        return userMapper.toProfileDto(userRepository.save(user));
+    }
+
+    @Override
+    public UserProfileDto updateUserRole(Long id, UpdateUserRoleDto updateDto) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new EntityNotFoundException("Cannot find user with id " + id)
+        );
+
+        userMapper.update(user, updateDto);
+        return userMapper.toProfileDto(user);
+    }
 }
