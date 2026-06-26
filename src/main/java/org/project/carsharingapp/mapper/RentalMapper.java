66920f9262260b345800d6d00603c7901ead0fed
Krupkolllia <krupkoilla397@gmail.com>
2026-06-26 23:36:23 +0200

    feat(rental): create rental mapper

--- src/main/java/org/project/carsharingapp/mapper/RentalMapper.java
+++ src/main/java/org/project/carsharingapp/mapper/RentalMapper.java
@@ -1,4 +1,13 @@
 package org.project.carsharingapp.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.project.carsharingapp.config.MapStructConfig;
+import org.project.carsharingapp.dto.rental.RentalResponseDto;
+import org.project.carsharingapp.model.rental.Rental;
+
+@Mapper(config = MapStructConfig.class)
 public interface RentalMapper {
+    @Mapping(target = "userId", source = "user.id")
+    RentalResponseDto toDto(Rental rental);
 }
