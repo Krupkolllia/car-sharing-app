b0e1c996f84f14ee49c40607a564b46d15c74255
Krupkolllia <krupkoilla397@gmail.com>
2026-06-26 14:58:19 +0200

    chore(norifications): create dummy notification service for future architecture

---
+++ src/main/java/org/project/carsharingapp/service/impl/NoOpNotificationService.java
@@ -0,0 +1,11 @@
+package org.project.carsharingapp.service.impl;
+
+import org.project.carsharingapp.service.NotificationService;
+
+public class NoOpNotificationService implements NotificationService {
+
+    @Override
+    public void sendNotification(String message) {
+
+    }
+}
