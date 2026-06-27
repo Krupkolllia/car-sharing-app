27492ebd3364ca2c6d5a52236e473dbcc0ff476a
Krupkolllia <krupkoilla397@gmail.com>
2026-06-27 02:51:01 +0200

    feat(exception): create NoAvailableCarsException

--- src/main/java/org/project/carsharingapp/exception/NoAvailableCarsException.java
+++ src/main/java/org/project/carsharingapp/exception/NoAvailableCarsException.java
@@ -1,7 +1,13 @@
 package org.project.carsharingapp.exception;
 
 public class NoAvailableCarsException extends RuntimeException {
-  public NoAvailableCarsException(String message) {
-    super(message);
-  }
+
+    public NoAvailableCarsException(String message) {
+        super(message);
+    }
+
+    public NoAvailableCarsException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
 }
