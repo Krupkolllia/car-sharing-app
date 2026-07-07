0641ad76c4caacd64f0a7c547f8ff31f27499e88
Krupkolllia <krupkoilla397@gmail.com>
2026-07-07 23:03:52 +0200

    feat(payment): add findAllFilteredByUserId method to repository

--- src/main/java/org/project/carsharingapp/repository/PaymentRepository.java
+++ src/main/java/org/project/carsharingapp/repository/PaymentRepository.java
@@ -1,8 +1,20 @@
 package org.project.carsharingapp.repository;
 
 import org.project.carsharingapp.model.payment.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    @EntityGraph(attributePaths = {"rental"})
+    @Query("""
+            SELECT p FROM Payment p
+            WHERE (:userId IS NULL OR p.rental.user.id = :userId)
+            """)
+    Page<Payment> findAllFilteredByUserId(@Param("userId") Long userId, Pageable pageable);
 
 }
---
+++ src/main/java/org/project/carsharingapp/service/StripePaymentService.java
@@ -0,0 +1,4 @@
+package org.project.carsharingapp.service;
+
+public class StripePaymentService {
+}
