66f395325b3824288653fdb3b896dbd71b729ce7
Krupkolllia <krupkoilla397@gmail.com>
2026-07-07 23:04:20 +0200

    feat(payment): create payment service

--- src/main/java/org/project/carsharingapp/service/PaymentService.java
+++ src/main/java/org/project/carsharingapp/service/PaymentService.java
@@ -1,4 +1,11 @@
 package org.project.carsharingapp.service;
 
+import org.project.carsharingapp.dto.payment.PaymentResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface PaymentService {
+
+    Page<PaymentResponseDto> findAll(Long userId, Pageable pageable);
+
 }
--- src/main/java/org/project/carsharingapp/service/StripePaymentService.java
+++ src/main/java/org/project/carsharingapp/service/StripePaymentService.java
@@ -1,4 +1,36 @@
 package org.project.carsharingapp.service;
 
-public class StripePaymentService {
+import lombok.RequiredArgsConstructor;
+import org.project.carsharingapp.dto.payment.PaymentResponseDto;
+import org.project.carsharingapp.mapper.PaymentMapper;
+import org.project.carsharingapp.model.user.Role;
+import org.project.carsharingapp.model.user.User;
+import org.project.carsharingapp.repository.PaymentRepository;
+import org.project.carsharingapp.security.SecurityUtil;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StripePaymentService implements PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    private final PaymentMapper paymentMapper;
+
+    @Transactional(readOnly = true)
+    @Override
+    public Page<PaymentResponseDto> findAll(Long userId, Pageable pageable) {
+        User currentUser = SecurityUtil.getAuthenticatedUser();
+
+        if (currentUser.getRole() == Role.CUSTOMER) {
+            userId = currentUser.getId();
+        }
+
+        return paymentRepository.findAllFilteredByUserId(userId, pageable)
+            .map(paymentMapper::toDto);
+    }
 }
