f1a5aa8556cedfcb62b48aa904d1ff0b7e8579cc
Krupkolllia <krupkoilla397@gmail.com>
2026-07-10 01:03:36 +0200

    feat(stripe): create stripe payment uri builder

--- src/main/java/org/project/carsharingapp/service/payment/PaymentUriBuilder.java
+++
@@ -1,5 +0,0 @@
-package org.project.carsharingapp.service.payment;
-
-public interface PaymentUriBuilder {
-
-}
--- src/main/java/org/project/carsharingapp/service/payment/StripePaymentUriBuilder.java
+++ src/main/java/org/project/carsharingapp/service/payment/StripePaymentUriBuilder.java
@@ -1,5 +1,36 @@
 package org.project.carsharingapp.service.payment;
 
+import lombok.RequiredArgsConstructor;
+import org.project.carsharingapp.properties.AppProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+@RequiredArgsConstructor
 public class StripePaymentUriBuilder {
+
+    private static final String SUCCESS_PATH = "/payments/success";
+    private static final String CANCEL_PATH = "/payments/cancel";
+    private static final String SESSION_ID_PARAM = "session_id";
+    private static final String CHECKOUT_SESSION_ID = "{CHECKOUT_SESSION_ID}";
+
+    private final AppProperties appProperties;
+
+    public String buildSuccessUrl() {
+        return UriComponentsBuilder
+            .fromUriString(appProperties.baseUrl())
+            .path(SUCCESS_PATH)
+            .queryParam(SESSION_ID_PARAM, CHECKOUT_SESSION_ID)
+            .build(false)
+            .toUriString();
+    }
+
+    public String buildCancelUrl() {
+        return UriComponentsBuilder
+            .fromUriString(appProperties.baseUrl())
+            .path(CANCEL_PATH)
+            .build()
+            .toUriString();
+    }
 
 }
