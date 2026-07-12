472c5f52faaae44621c80e7a3976e81dad45a1ee
Krupkolllia <krupkoilla397@gmail.com>
2026-07-12 17:41:30 +0200

    feat(notification): make notification event listener async

--- src/main/java/org/project/carsharingapp/service/notifications/NotificationEventListener.java
+++ src/main/java/org/project/carsharingapp/service/notifications/NotificationEventListener.java
@@ -1,6 +1,7 @@
 package org.project.carsharingapp.service.notifications;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -11,13 +12,14 @@ public class NotificationEventListener {
 
     private final NotificationService notificationService;
 
+    @Async("externalApiExecutor")
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(NotificationRequestedEvent event) {
         try {
             notificationService.sendNotification(event.message());
         } catch (Exception e) {
             System.err.println("Failed to send notification: \n" + event.message()
-                + "\n" + e.getMessage());
+                    + "\n" + e.getMessage());
         }
     }
 
