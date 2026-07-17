5a3dbfb2f44695bfb95e05a9f822c6cfbdf410c1
Krupkolllia <krupkoilla397@gmail.com>
2026-07-18 01:00:12 +0200

    fix(rental): change @FutureOrPresent to @Future in rental request dto

--- src/main/java/org/project/carsharingapp/dto/rental/RentalRequestDto.java
+++ src/main/java/org/project/carsharingapp/dto/rental/RentalRequestDto.java
@@ -1,12 +1,12 @@
 package org.project.carsharingapp.dto.rental;
 
-import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record RentalRequestDto(
         @NotNull
-        @FutureOrPresent
+        @Future
         LocalDate returnDate,
 
         @NotNull
