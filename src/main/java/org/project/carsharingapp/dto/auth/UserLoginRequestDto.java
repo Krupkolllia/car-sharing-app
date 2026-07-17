38f72e76e80e8a443ea6e063ca9197611b0f88bd
Krupkolllia <krupkoilla397@gmail.com>
2026-07-17 22:18:42 +0200

    chore(liquibase): add missing db checks

--- src/main/java/org/project/carsharingapp/dto/auth/UserLoginRequestDto.java
+++ src/main/java/org/project/carsharingapp/dto/auth/UserLoginRequestDto.java
@@ -2,13 +2,13 @@ package org.project.carsharingapp.dto.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Size;
 
 public record UserLoginRequestDto(
         @NotBlank
         @Email
         String email,
         @NotBlank
-        @Length(min = 8)
+        @Size(min = 8)
         String password
 ) {}
