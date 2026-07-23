29fc3d5945652a1dee961fafbad23aa0323d642d
Krupkolllia <krupkoilla397@gmail.com>
2026-07-23 14:35:29 +0200

    chore(app): enable support for paging

--- src/main/java/org/project/carsharingapp/CarSharingAppApplication.java
+++ src/main/java/org/project/carsharingapp/CarSharingAppApplication.java
@@ -3,8 +3,12 @@ package org.project.carsharingapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableSpringDataWebSupport(
+        pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO
+)
 @EnableScheduling
 @ConfigurationPropertiesScan
 @SpringBootApplication
